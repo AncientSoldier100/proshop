121d696db71b6c501b2b131e146c4fb45fcfc99b
Aarav Jha <aarav_jha2007@outlook.com>
2021-02-12 16:05:58 +0400

    Product Screen and routing to product screen, cart and login page with React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreens from "./screens/HomeScreens";
+import ProductScreens from "./screens/ProductScreens";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreens />
+          <Route path="/" component={HomeScreens} exact />
+          <Route path="/product/:id" component={ProductScreens} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
